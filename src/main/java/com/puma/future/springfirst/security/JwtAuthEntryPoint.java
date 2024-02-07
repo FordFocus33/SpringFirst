ce2a3831181ccc59cbdd340843f0ac87ece66b83
Boss <wbc123454321@gmail.com>
2024-02-07 16:37:49 +0300

    Update

--- src/main/java/com/puma/future/springfirst/security/JwtAuthEntryPoint.java
+++ src/main/java/com/puma/future/springfirst/security/JwtAuthEntryPoint.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 @Component
 public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
 
-    // Класс для вывода ошибки об неавторизованности
+    // Класс для вывода ошибки об неаутентификации
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
